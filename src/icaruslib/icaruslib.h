ec65aa809589c45a85cd6d5798ad8269bc6d2ef2
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2015-05-21 01:36:27 -0300

    Creating a directory structure for the main lib icaruslib who will coordinate all the sensor of the telemetry system.

---
+++ src/icaruslib/icaruslib.h
@@ -0,0 +1,23 @@
+#ifndef _ICARUSLIB_H
+#define _ICARUSLIB_H
+
+// Arduino Library
+#include <Arduino.h>
+
+// C++ Headers
+#include <iostream>
+#include <vector>
+
+//Sensors Headers
+#include "IFS_MPU6050.h"
+#include "IFS_MLX90614.h"
+#include "IFS_US1881.h"
+#include "IFS_DS18B20.h"
+#include "IFS_GPS.h"
+
+class icaruslib {
+
+};
+
+
+#endif
