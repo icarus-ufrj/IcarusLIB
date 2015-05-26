100bc7aa77cb80378ca378d7bc1d75fb775d698a
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2015-05-26 20:59:35 -0300

    Creating new methods for the Class of the sensor Hall.

--- src/IFS_US1881/IFS_US1881.cpp
+++ src/IFS_US1881/IFS_US1881.cpp
@@ -1,1 +1,26 @@
 #include "IFS_US1881.h"
+namespace IcarusLib{
+
+IFS_US1881(unsigned magnets) :
+	magnets(magnets)
+{
+	startTime = micros();
+}
+
+void IFS_US1881::setPort(const unsigned &value){
+	pin = value;
+}
+
+void IFS_US1881::getChangeState(){
+ 	counterMagnet++;
+}
+
+unsigned IFS_US1881::getRPM(){
+	return RPM;
+}
+
+void IFS_US1881::calculateRPM(){
+
+}
+
+}
