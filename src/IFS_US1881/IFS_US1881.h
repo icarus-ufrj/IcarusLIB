05ea0dd69ab8aada8a0ca09c97d392a77f44855f
Hguimaraes <hguimaraes@poli.ufrj.br>
2015-05-26 16:45:58 -0300

    Modeling the Hall sensor.

--- src/IFS_US1881/IFS_US1881.h
+++ src/IFS_US1881/IFS_US1881.h
@@ -1,9 +1,26 @@
 #ifndef _IFS_US1881_H_
 #define _IFS_US1881_H_
 
+#include "Arduino.h"
+
 namespace IcarusLib{
 class IFS_US1881 {
+	unsigned pin;
+	unsigned RPM;
+	volatile unsigned counterMagnet;
 
+public:
+	IFS_US1881();
+	virtual ~IFS_US1881();
+
+	// Function to set the pin associated to the sensor
+	void setPort(const unsigned &value);
+
+	// Function to gets a signal change in a specific port
+	void getChangeState();
+
+	// Function to read the RPM from a magnet
+	unsigned readRPM();
 };
 }
 
