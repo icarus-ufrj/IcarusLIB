2210b66e4f12d27730f2451ba52ab928f0719768
Hguimaraes <hguimaraes@poli.ufrj.br>
2015-05-26 16:46:35 -0300

    Modeling the MPU6050

--- src/IFS_MPU6050/IFS_MPU6050.h
+++ src/IFS_MPU6050/IFS_MPU6050.h
@@ -1,10 +1,17 @@
 #ifndef _IFS_MPU6050_H_
 #define _IFS_MPU6050_H_
 
-#include <MPU6050.h>
+#include "Arduino.h"
+#include "MPU6050.h"
 
 namespace IcarusLib{
-class IFS_MPU6050 : public MPU6050 {
+class IFS_MPU6050 {
+	MPU6050 *gyroAccel = NULL;
+
+public:
+	IFS_MPU6050();
+	virtual ~IFS_MPU6050();
+
 
 };
 }
